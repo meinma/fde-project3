9b2752dd06e8f2c022ab87e7f300a24e58880c88
Timo Kersten <kersten@in.tum.de>
2019-02-08 14:15:25 +0100

    Speed up parsing

--- src/DistCalculator.cpp
+++ src/DistCalculator.cpp
@@ -7,80 +7,45 @@
 #include <fstream>
 #include <unordered_set>
 
-DistCalculator::DistCalculator(std::string edgeListFile) {
-    std::ifstream stream;
-    stream.open(edgeListFile, std::ios::in);
-    if (stream.is_open()) {
-        int actorId = 0;
-        int movieId = 0;
-        std::unordered_set<int> actors;
-        std::unordered_set<int> movies;
-        std::string line;
-        for (int i = 0; i < 2; i++) {
-            int counter = 0;
-            // Read line by line of the input file
-            while (std::getline(stream, line)) {
-                // Skip the first line because there's no relevant data
-                if (counter == 0) {
-                    counter++;
-                    continue;
-                }
+DistCalculator::DistCalculator(std::string edgeListFile)
+{
+   std::ifstream stream;
+   stream.open(edgeListFile, std::ios::in);
+   if (stream.is_open()) {
+      int actorId = 0;
+      int movieId = 0;
+      int maxActorId = 0;
+      int maxMovieId = 0;
+      std::string line;
+      while (std::getline(stream, line)) maxActorId++;
+      maxMovieId = maxActorId;
 
-                // Get the actorId of the line --> funktioniert korrekt
-                const char *data = line.data(), *limit = data + line.length(), *last = data;
-                unsigned field = 0;
-                for (auto iter = data; iter != limit; ++iter) {
-                    if ((*iter) == ',') {
-                        if (++field == 1) {
-                            union {
-                                unsigned v;
-                                char buffer[sizeof(unsigned)];
-                            };
-                            v = 0;
-                            for (auto iter2 = last; iter2 != iter; ++iter2)
-                                v = 10 * v + (*iter2) - '0';
-                            actorId = v;
-                            break;
+      actorMovies.resize(std::max(maxActorId + 1, 50));
+      movieActors.resize(std::max(maxMovieId + 1, 50));
+      stream.clear();
+      stream.seekg(0, std::ios::beg);
+      int counter = 0;
+      // Skip first line
+      std::getline(stream, line);
+      // Read line by line of the input file
+      while (std::getline(stream, line)) {
+         // Get the actorId of the line
+         const char *iter = line.data(), *limit = iter + line.length();
+         unsigned v = 0;
+         for (; *iter != ','; ++iter) v = 10 * v + (*iter) - '0';
+         actorId = v;
+         // Skip ','
+         iter++;
+         v = 0;
+         // Get the movieId of the line
+         for (; iter != limit; ++iter) v = 10 * v + (*iter) - '0';
+         movieId = v;
 
-                        } else
-                            last = iter + 1;
-                    }
-                }
-
-                const char *data2 = line.data(), *limit2 = data + line.length(), *last2 = data;
-                unsigned field2 = 0;
-                for (auto iter3 = data2; iter3 != limit2; ++iter3) {
-                    if ((*iter3) == ',') {
-                        if (++field2 == 1) {
-                            std::string w(iter3);
-                            movieId = std::stoi(w.substr(1, w.size()));
-                            break;
-                        }
-                    }
-                }
-
-                //Jede ActorId wird gespeichert um später die Anzahl der unterschiedlichen Actors zu haben im ersten Durchgang
-                if (i == 0) {
-                    actors.insert(actorId);
-                    movies.insert(movieId);
-                }
-                    // Im zweiten Durchgang füllen der Vectoren
-                else {
-                    actorMovies[actorId].push_back(movieId);
-                    movieActors[movieId].push_back(actorId);
-                }
-            }
-            // Nach dem ersten Durchlauf
-            if (i == 0) {
-                int distinctActors = actors.size();
-                int distinctMovies = movies.size();
-                actorMovies.resize(distinctActors + 1);
-                movieActors.resize(distinctMovies + 1);
-                stream.clear();
-                stream.seekg(0, std::ios::beg);
-            }
-        }
-    }
+         actorMovies[actorId].push_back(movieId);
+         movieActors[movieId].push_back(actorId);
+      }
+      // Nach dem ersten Durchlauf
+   }
 }
 
 
